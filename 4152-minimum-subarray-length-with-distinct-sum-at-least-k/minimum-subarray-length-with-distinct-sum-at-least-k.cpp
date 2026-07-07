ff4c07550123b5bc5895ed2c563cab0c6faa3d7c
malhotra2701 <bt25eci068@iiitn.ac.in>
2026-07-08 04:35:02 +0530

    Time: 407 ms (39.90%) | Memory: 318.1 MB (30.21%) - LeetSync

--- 4152-minimum-subarray-length-with-distinct-sum-at-least-k/minimum-subarray-length-with-distinct-sum-at-least-k.cpp
+++ 4152-minimum-subarray-length-with-distinct-sum-at-least-k/minimum-subarray-length-with-distinct-sum-at-least-k.cpp
@@ -1,11 +1,11 @@
 class Solution {
 public:
     int minLength(vector<int>& nums, int k) {
-        unordered_map<int,int> mp;
+        int sum = 0;
         int n = nums.size();
         int left = 0;
-        long long sum = 0;
         int res = INT_MAX;
+        unordered_map<int,int> mp;
         for(int right=0;right<n;right++){
             mp[nums[right]]++;
             if(mp[nums[right]]==1){
@@ -13,11 +13,9 @@ public:
             }
             while(sum>=k && left<=right){
                 res = min(res,right-left+1);
-                if(mp[nums[left]]==1){
-                    sum = sum - nums[left];
-                }
                 mp[nums[left]]--;
                 if(mp[nums[left]]==0){
+                    sum = sum - nums[left];
                     mp.erase(nums[left]);
                 }
                 left++;
