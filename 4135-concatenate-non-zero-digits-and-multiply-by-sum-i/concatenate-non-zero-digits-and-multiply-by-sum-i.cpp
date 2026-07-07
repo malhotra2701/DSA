4bad6a2d752d116e76d8c4ea2c6f6e6b665eee53
malhotra2701 <bt25eci068@iiitn.ac.in>
2026-07-07 18:10:48 +0530

    Time: 0 ms (100.00%) | Memory: 9.5 MB (6.15%) - LeetSync

--- 4135-concatenate-non-zero-digits-and-multiply-by-sum-i/concatenate-non-zero-digits-and-multiply-by-sum-i.cpp
+++ 4135-concatenate-non-zero-digits-and-multiply-by-sum-i/concatenate-non-zero-digits-and-multiply-by-sum-i.cpp
@@ -3,19 +3,19 @@ public:
     long long sumAndMultiply(int n) {
         string res = "";
         while(n!=0){
-            if(n%10 !=0){
-                res=res+(to_string)(n%10);
-            }
+            if(n%10 != 0)
+            res = res + to_string(n%10);
             n=n/10;
         }
-        if(res==""){
+        reverse(res.begin(),res.end());
+        if(res == ""){
             return 0;
         }
         long long sum = 0;
         for(int i=0;i<res.size();i++){
             sum = sum + (res[i]-'0');
         }
-        reverse(res.begin(),res.end());
-        return sum*(stoll(res));
+        long long x = stoll(res);
+        return x*sum;
     }
 };
