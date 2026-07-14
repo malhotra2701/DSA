b833ece67d669b332c56816d4cd1534ec4e22a6f
malhotra2701 <bt25eci068@iiitn.ac.in>
2026-07-14 05:52:47 +0530

    Time: 3 ms (20.71%) | Memory: 23.7 MB (12.63%) - LeetSync

--- 3330-modify-the-matrix/modify-the-matrix.cpp
+++ 3330-modify-the-matrix/modify-the-matrix.cpp
@@ -1,23 +1,20 @@
 class Solution {
 public:
-    vector<vector<int>> modifiedMatrix(vector<vector<int>>& arr) {
-        int m = arr.size();
-        int n = arr[0].size();
-        vector<int> temp;
-        for(int i=0;i<n;i++){
+    vector<vector<int>> modifiedMatrix(vector<vector<int>>& mat) {
+        // column traversal
+        int m = mat.size();
+        int n = mat[0].size();
+        for(int j=0;j<n;j++){
             int mx = INT_MIN;
-            for(int j=0;j<m;j++){
-                mx = max(mx,arr[j][i]);
+            for(int i=0;i<m;i++){
+                mx = max(mx,mat[i][j]);
             }
-            temp.push_back(mx);
-        }
-        for(int i=0;i<n;i++){
-            for(int j=0;j<m;j++){
-                if(arr[j][i]==-1){
-                    arr[j][i]=temp[i];
+            for(int i=0;i<m;i++){
+                if(mat[i][j]==-1){
+                    mat[i][j]=mx;
                 }
             }
         }
-        return arr;
+        return mat;
     }
 };
