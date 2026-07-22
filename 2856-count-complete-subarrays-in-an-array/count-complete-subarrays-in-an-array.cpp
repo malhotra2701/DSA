7537e44c7afdad91f3193b45f538aeba3e61ebc4
malhotra2701 <bt25eci068@iiitn.ac.in>
2026-07-22 19:01:47 +0530

    Time: 13 ms (86.92%) | Memory: 41.9 MB (65.02%) - LeetSync

--- 2856-count-complete-subarrays-in-an-array/count-complete-subarrays-in-an-array.cpp
+++ 2856-count-complete-subarrays-in-an-array/count-complete-subarrays-in-an-array.cpp
@@ -1,30 +1,26 @@
 class Solution {
 public: 
-    int helper(vector<int> &nums,int k){
-        unordered_map<int,int> mp;
-        int count = 0;
-        int n = nums.size();
-        int left = 0;
-        for(int right=0;right<n;right++){
-            mp[nums[right]]++;
-            while(mp.size()>k && left<=right){
-                mp[nums[left]]--;
-                if(mp[nums[left]]==0){
-                    mp.erase(nums[left]);
-                }
-                left++;
-            }
-            count += right - left + 1;
-        }
-        return count;
-    }
     int countCompleteSubarrays(vector<int>& nums) {
         int n = nums.size();
         unordered_map<int,int> mp;
         for(int i=0;i<n;i++){
             mp[nums[i]]++;
         }
-        int k = mp.size();
-        return helper(nums,k) - helper(nums,k-1);
+        int k = mp.size() - 1;
+        int left = 0;
+        unordered_map<int,int> m;
+        int count = 0;
+        for(int right=0;right<n;right++){
+            m[nums[right]]++;
+            while(m.size()>k && left<=right){
+                m[nums[left]]--;
+                if(m[nums[left]]==0){
+                    m.erase(nums[left]);
+                }
+                left++;
+            }
+            count += right-left+1;
+        }
+        return ((n)*(n+1))/2 - count;
     }
 };
