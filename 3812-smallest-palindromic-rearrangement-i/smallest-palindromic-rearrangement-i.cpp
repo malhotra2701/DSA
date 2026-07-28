15e6686f917472d55c221274c812f8286cde2bb8
malhotra2701 <bt25eci068@iiitn.ac.in>
2026-07-28 16:54:03 +0530

    Time: 6 ms (99.78%) | Memory: 54.3 MB (88.13%) - LeetSync

--- 3812-smallest-palindromic-rearrangement-i/smallest-palindromic-rearrangement-i.cpp
+++ 3812-smallest-palindromic-rearrangement-i/smallest-palindromic-rearrangement-i.cpp
@@ -9,24 +9,13 @@ public:
         int i=0;
         int j=n-1;
         for(int k=0;k<temp.size();k++){
-            if(temp[k]>1 && temp[k]%2==0){
-                while(temp[k]!=0){
-                    s[i]=(char)('a' + k);
-                    s[j]=(char)('a' + k);
-                    i++;
-                    j--;
-                    temp[k] -= 2;
-                }
-            }
-            else if(temp[k]>1 && temp[k]%2!=0){
-                while(temp[k]!=1){
-                    s[i]=(char)('a' + k);
-                    s[j]=(char)('a' + k);
-                    i++;
-                    j--;
-                    temp[k] -= 2;
-                }
-            }
+           while(temp[k]>=2){
+            s[i]=(char)(k+'a');
+            s[j]=(char)(k+'a');
+            i++;
+            j--;
+            temp[k] -= 2;
+           }
         }
         for(int i=0;i<temp.size();i++){
             if(temp[i]==1){
