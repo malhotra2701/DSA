0bdae0c62addb36f4a4add44372cbdc73980ca85
malhotra2701 <bt25eci068@iiitn.ac.in>
2026-07-31 05:29:36 +0530

    Time: 145 ms (29.85%) | Memory: 118.8 MB (91.85%) - LeetSync

--- 2338-minimum-consecutive-cards-to-pick-up/minimum-consecutive-cards-to-pick-up.cpp
+++ 2338-minimum-consecutive-cards-to-pick-up/minimum-consecutive-cards-to-pick-up.cpp
@@ -1,14 +1,14 @@
 class Solution {
 public:
     int minimumCardPickup(vector<int>& nums) {
-        unordered_map<int,int> mp;
         int n = nums.size();
+        unordered_map<int,int> mp;
         int left = 0;
         int res = INT_MAX;
-        for(int right = 0;right<n;right++){
+        for(int right=0;right<n;right++){
             mp[nums[right]]++;
             while(mp[nums[right]]==2 && left<=right){
-                res = min(res , right-left+1);
+                res = min(res,right-left+1);
                 mp[nums[left]]--;
                 if(mp[nums[left]]==0){
                     mp.erase(nums[left]);
