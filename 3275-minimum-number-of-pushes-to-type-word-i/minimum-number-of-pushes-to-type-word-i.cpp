e6986fcf7c6550aaee41b057541f4de4d37cdeb9
malhotra2701 <bt25eci068@iiitn.ac.in>
2026-07-30 05:56:19 +0530

    Time: 0 ms (100.00%) | Memory: 9.3 MB (11.93%) - LeetSync

--- 3275-minimum-number-of-pushes-to-type-word-i/minimum-number-of-pushes-to-type-word-i.cpp
+++ 3275-minimum-number-of-pushes-to-type-word-i/minimum-number-of-pushes-to-type-word-i.cpp
@@ -5,24 +5,20 @@ public:
         for(auto it : word){
             mp[it]++;
         }
-        vector<pair<char,int>> f(mp.begin(),mp.end());
-        sort(f.begin(),f.end(),[](pair<char,int> &a,pair<char,int> &b){
-            return a.second>b.second;
-        });
         int i = 0;
         int res = 0;
-        while(i<f.size()){
+        while(i<mp.size()){
             if(i<=7){
-                res = res + f[i].second;
+                res++;
             }
             else if(i>=8 && i<=15){
-                res = res + (f[i].second)*2;
+                res += 2;
             }
             else if(i>=16 && i<=23){
-                res = res + (f[i].second)*3;
+                res += 3;
             }
             else{
-                res = res + (f[i].second)*4;
+                res += 4;
             }
             i++;
         }
