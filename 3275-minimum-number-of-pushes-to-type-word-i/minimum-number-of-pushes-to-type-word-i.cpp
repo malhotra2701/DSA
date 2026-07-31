4d46d17d3071f04a2ac5518c8b474066dc48a46a
malhotra2701 <bt25eci068@iiitn.ac.in>
2026-07-31 05:36:31 +0530

    Time: 0 ms (100.00%) | Memory: 8.2 MB (77.71%) - LeetSync

--- 3275-minimum-number-of-pushes-to-type-word-i/minimum-number-of-pushes-to-type-word-i.cpp
+++ 3275-minimum-number-of-pushes-to-type-word-i/minimum-number-of-pushes-to-type-word-i.cpp
@@ -1,27 +1,18 @@
 class Solution {
 public:
     int minimumPushes(string word) {
-        unordered_map<char,int> mp;
-        for(auto it : word){
-            mp[it]++;
+        int n = word.size();
+        if(n<=8){
+            return n;
         }
-        int i = 0;
-        int res = 0;
-        while(i<mp.size()){
-            if(i<=7){
-                res++;
-            }
-            else if(i>=8 && i<=15){
-                res += 2;
-            }
-            else if(i>=16 && i<=23){
-                res += 3;
-            }
-            else{
-                res += 4;
-            }
-            i++;
+        else if(n>=9 && n<=16){
+            return 8 + (n-8)*2;
         }
-        return res;
+        else if(n>=17 && n<=24){
+            return 24 + (n-16)*3;
+        }
+        else{
+            return 48 + (n-24)*4;
+        }
     }
 };
