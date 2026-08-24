9836f5401964618e0ac33dbe85f86722c71186c6
malhotra2701 <bt25eci068@iiitn.ac.in>
2026-08-24 20:18:42 +0530

    Time: 44 ms (69.02%) | Memory: 243.4 MB (74.11%) - LeetSync

--- 4349-minimum-initial-strength-to-defeat-all-monsters/minimum-initial-strength-to-defeat-all-monsters.cpp
+++ 4349-minimum-initial-strength-to-defeat-all-monsters/minimum-initial-strength-to-defeat-all-monsters.cpp
@@ -1,47 +1,46 @@
 class Solution {
 public:
-    bool helper(long long strength,vector<int> &monsters,vector<long long> &temp){
-        int s = monsters.size();
-        for(int i=0;i<s;i++){
+    bool checker(long long strength,vector<long long> &temp,vector<int> &monsters){
+        for(int i=0;i<monsters.size();i++){
+            if(strength<0){
+                strength = 0;
+            }
             if(strength + temp[i] < monsters[i]){
                 return false;
             }
             strength = strength - monsters[i];
-            if(strength <= 0){
-                strength = 0;
-            }
         }
         return true;
     }
     long long minInitialStrength(vector<int>& monsters, vector<vector<int>>& boosts) {
-        int s = monsters.size();
-        int n = boosts.size();
-        vector<long long> temp(s,0);
-        for(int i=0;i<n;i++){
+        int n = monsters.size();
+        int s = boosts.size();
+        vector<long long> temp(n,0);
+        for(int i=0;i<s;i++){
             int start = boosts[i][0];
             int end = boosts[i][1];
             temp[start] += boosts[i][2];
-            if(end<s-1){
+            if(end<n-1){
                 temp[end+1] -= boosts[i][2];
             }
         }
-        for(int i=1;i<s;i++){
+        for(int i=1;i<n;i++){
             temp[i] += temp[i-1];
         }
         long long low = 0;
         long long high = 0;
-        for(int i=0;i<s;i++){
+        for(int i=0;i<n;i++){
             high += monsters[i];
         }
         long long ans = high;
         while(low<=high){
-            long long mid = low + (high-low)/2;
-            if(helper(mid,monsters,temp)){
-                high = mid-1;
+            long long mid = low + (high - low)/2;
+            if(checker(mid,temp,monsters)){
                 ans = mid;
+                high = mid -1;
             }
             else{
-                low = mid+1;
+                low = mid + 1;
             }
         }
         return ans;
