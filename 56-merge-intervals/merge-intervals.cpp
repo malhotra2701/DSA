ff8d0147d437c9302a7d0e4080ac19ffabf7362f
malhotra2701 <bt25eci068@iiitn.ac.in>
2026-09-13 00:13:11 +0530

    Time: 7 ms (39.49%) | Memory: 23.9 MB (60.28%) - LeetSync

--- 56-merge-intervals/merge-intervals.cpp
+++ 56-merge-intervals/merge-intervals.cpp
@@ -4,21 +4,19 @@ public:
         int n = arr.size();
         vector<vector<int>> ans;
         sort(arr.begin(),arr.end());
-        for(int i=0;i<n;i++){
+        ans.push_back({arr[0][0],arr[0][1]});
+        for(int i=1;i<n;i++){
             int start = arr[i][0];
             int end = arr[i][1];
-            if(ans.empty()){
-                ans.push_back({start,end});
+            if(start<=ans.back()[1]){
+                ans.back()[1] = max(ans.back()[1],end);
             }
             else{
-                if(start<=ans.back()[1]){
-                    ans.back()[1]= max(ans.back()[1],end);
-                }
-                else{
-                    ans.push_back({start,end});
-                }
+                ans.push_back({start,end});
             }
         }
         return ans;
     }
 };
+//TC O(nLOGN) + O(N) -> O(Nlogn)
+//SC O(N)
